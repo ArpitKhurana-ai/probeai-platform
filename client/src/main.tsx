0988743b4b3d1762b90ccfcf1981d3bfd8e545a1
Arpit Khurana <arpitkhurana155@gmail.com>
2025-07-10 20:46:00 +0530

    🚑 Fix: Restore working routing and render logic using ReactDOM and wouter only

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { render } from "preact";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-render(<App />, document.getElementById("root")!);
+ReactDOM.render(<App />, document.getElementById("root"));
